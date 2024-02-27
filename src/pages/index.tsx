31c434a74eb14cffdb3a765fbe223b4fbb9224a2
Aastik <aastikdude7@gmail.com>
2024-02-27 22:12:48 +0530

    vercel env updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,9 +15,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      
         <div>
-          <h1></h1>
-          <SignInButton />
+         
+        <SignInButton />
         </div>
       </main>
     </>
